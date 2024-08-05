d71f5e0384bedef4a044b547ccd06ba80c92de9c
Jennifer <jennifer15aries@gmail.com>
2024-08-04 21:44:51 -0500

    user interface

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+//componentes PasCal
+import './App.css'
+export function App(){
+    return(
+        <article>
+            <header>
+                <img src="https://unavatar.io/kikobeats?ttl=1h" alt="avatar" />
+                <div>
+                    <strong>Jennifer Chileno Santisteban</strong>
+                    <span>@J3nn1Cs</span>
+                </div>
+            </header>
+
+            <aside>
+                <button>Seguir</button>
+            </aside>
+        </article>
+    )
+}
